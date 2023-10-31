3e42170432cc77496f2c2b64c21702c9ee185408
Errons1 <snorreledal@hotmail.no>
2023-10-31 19:45:37 +0100

    changed some names

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,30 +1,44 @@
 package com.example.demo;
 
 import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
-@AllArgsConstructor
 @RestController
-public class AppController {
+public class AppController implements ApplicationListener<ApplicationReadyEvent> {
     
-    private MeterRegistry meterRegistry;
-    private static int postCounter;
+    private final MeterRegistry meterRegistry;
+//    private static long counter;
+    private final Map<String, Long> map = new HashMap<>();
     
+    public AppController(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+        map.put("counter", 0L);
+    }
+
     @Timed
     @GetMapping("/")
     public ResponseEntity<String> helloWorld() {
         log.info("GET helloWorld");  
-        meterRegistry.counter("get_hello_world").increment();
-        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+        meterRegistry.counter("get_hello").increment();
+        MultiValueMap<String, String> map = new HttpHeaders();
+        map.add("Connection", "close");
+        return new ResponseEntity<>("Hello World", map, HttpStatus.OK);
     }
     
     @Timed
@@ -36,9 +50,16 @@ public class AppController {
         }
         
         log.info("POST counter");
-        postCounter += amount;
-        meterRegistry.gauge("counter", postCounter);
+        map.put("counter", map.get("counter"));
+//        meterRegistry.gauge("post_hello", counter);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        // Verdi av total
+        Gauge.builder("post_hello", map, m -> m.get("counter")).register(meterRegistry);
+    }
+    
 
 }
--- terraform/dashboard.tf
+++ terraform/dashboard.tf
@@ -14,15 +14,59 @@ resource "aws_cloudwatch_dashboard" "dashboard" {
           metrics = [
             [
               "${ var.prefix }-dashboard",
-              "get_hello_world.count"
+              "get_hello.count"
             ]
           ]
           period = 60
           stat   = "Sum"
           region = "eu-west-1"
-          title  = "How many GET received"
+          title  = "How many GET served"
+        }
+      },
+      {
+        type   = "number"
+        x      = 6
+        y      = 0
+        width  = 6
+        height = 6
+
+        properties = {
+          metrics = [
+            [
+              "${ var.prefix }-dashboard",
+              "post_hello.count"
+            ]
+          ]
+          sparkline            = true,
+          view                 = "singleValue",
+          stat                 = "Sum"
+          period               = 60
+          setPeriodToTimeRange = true
+          region               = "eu-west-1"
+          title                = "How many POST received"
         }
       }
+      #      {
+      #        type   = "number"
+      #        x      = 6
+      #        y      = 0
+      #        width  = 6
+      #        height = 6
+      #        
+      #        properties = {
+      #          metrics = [
+      #            [
+      #              "${ var.prefix }-dashboard",
+      #              "post_hello.count"
+      #            ]
+      #          ]
+      #          sparkline = true,
+      #          view      = "singleValue",
+      #          region    = "eu-west-1",
+      #          stat      = "Sum",
+      #          period    = 60
+      #        }
+      #      }
     ]
   })
 }
