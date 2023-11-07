c5e4e447c5e6a9dd8eee896d02c9424c4bc0c108
Snorre Ledal <snorreledal@hotmail.no>
2023-11-07 09:48:46 +0100

    Testing local alarm module

---
+++ aws_alarm/variables.tf
@@ -0,0 +1,13 @@
+variable "prefix" {
+  default = "prefix"
+  type = string
+}
+
+variable "threshold" {
+  default = "10"
+  type = string
+}
+
+variable "alarm_email" {
+  type = string
+}
--- terraform/dashboard.tf
+++ terraform/dashboard.tf
@@ -22,51 +22,14 @@ resource "aws_cloudwatch_dashboard" "dashboard" {
           region = "eu-west-1"
           title  = "How many GET served"
         }
-      },
-      {
-        type   = "number"
-        x      = 6
-        y      = 0
-        width  = 6
-        height = 6
-
-        properties = {
-          metrics = [
-            [
-              "${ var.prefix }-dashboard",
-              "post_hello.count"
-            ]
-          ]
-          sparkline            = true,
-          view                 = "singleValue",
-          stat                 = "Sum"
-          period               = 60
-          setPeriodToTimeRange = true
-          region               = "eu-west-1"
-          title                = "How many POST received"
-        }
       }
-      #      {
-      #        type   = "number"
-      #        x      = 6
-      #        y      = 0
-      #        width  = 6
-      #        height = 6
-      #        
-      #        properties = {
-      #          metrics = [
-      #            [
-      #              "${ var.prefix }-dashboard",
-      #              "post_hello.count"
-      #            ]
-      #          ]
-      #          sparkline = true,
-      #          view      = "singleValue",
-      #          region    = "eu-west-1",
-      #          stat      = "Sum",
-      #          period    = 60
-      #        }
-      #      }
     ]
   })
+}
+
+module "alarm" {
+  source = "../aws_alarm"
+  prefix = "snle"
+  alarm_email = "snorre@hotmail.no"
+  threshold = "5"
 }
